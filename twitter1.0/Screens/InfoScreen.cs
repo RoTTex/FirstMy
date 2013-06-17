5cf7cf5858e25b033925ecec7032c44a78f5c8e8
Maxim Smirnow <maxim.smirnow@touchin.com>
2013-06-17 15:28:29 +0400

    Исправил InfoScreen: вынес кнопки из функции, поставил задний фон.

--- twitter1.0/Screens/InfoScreen.cs
+++ twitter1.0/Screens/InfoScreen.cs
@@ -7,7 +7,10 @@ namespace Twitter
 {
 	public partial class InfoScreen : UIViewController
 	{
-		public InfoScreen () : base ("InfoScreen", null)
+		private UIButton _btnCall = UIButton.FromType (UIButtonType.Custom);
+		private UIButton _btnMail = UIButton.FromType (UIButtonType.Custom);
+
+		public InfoScreen () : base ()
 		{
 		}
 
@@ -19,50 +22,57 @@ namespace Twitter
 		public override void ViewDidLoad ()
 		{
 			base.ViewDidLoad ();
-			
+
+			AddComponents ();
+		}
+
+		private void AddComponents()
+		{
+			var imgBackground = new UIImage (@"Tweets/bg.png").StretchableImage (50, 0);
+			var imgViewBackground = new UIImageView (imgBackground);
+			View.AddSubview (imgViewBackground);
+
 			var img = new UIImage (@"info/logo.png");
 			var imgView = new UIImageView(img);
-			imgView.Frame = new RectangleF (0, 20, 320, img.Size.Height);
+			imgView.Frame = new RectangleF (0, 5, 320, img.Size.Height);
 			imgView.ContentMode = UIViewContentMode.Center;
 			Add (imgView);
 
-			var text = new UITextView (new RectangleF(10, img.Size.Height + 30, 300, 200));
+			var text = new UITextView (new RectangleF(10, imgView.Frame.Bottom + 1, 300, 180));
 			text.Font = UIFont.FromName("HelveticaNeue", 13);
-			text.Text = "Нам не стыдно за выпускаемые продукты, все они сделаны с вниманием к деталям. Пользователи это ценят, многие наши приложения попадают в топы AppStore и получают высокие оценки. \n\nМы любим своих заказчиков и решаем их задачи. На письма и телефон отвечаем быстро, по праздникам и выходным, делаем работу в срок и никуда не пропадаем.\nЗакажите разработку сейчас! ";
+			text.Text = "Нам не стыдно за выпускаемые продукты, все они сделаны с вниманием к деталям. Пользователи это ценят, многие наши приложения попадают в топы AppStore и получают высокие оценки. \nМы любим своих заказчиков и решаем их задачи. На письма и телефон отвечаем быстро, по праздникам и выходным, делаем работу в срок и никуда не пропадаем.\nЗакажите разработку сейчас! ";
 			text.TextAlignment = UITextAlignment.Left;
-			text.TextColor = UIColor.Black;
+			text.TextColor = UIColor.FromRGB (65, 65, 65);
 			text.ScrollEnabled = true;
+			text.UserInteractionEnabled = false;
+			text.BackgroundColor = new UIColor (0, 0, 0, 0);
 			Add (text);
 
-			var imgCallButton = new UIImage (@"Info/button.png").StretchableImage (11, 0);
-			var imgCallButton_pressed = new UIImage (@"Info/button_pressed.png").StretchableImage (11, 0);
+			var imgButton = new UIImage (@"Info/button.png").StretchableImage (11,0);
+			var imgButton_pressed = new UIImage (@"Info/button_pressed.png").StretchableImage (11,0);
+
 			var imgCallButtonIcon = new UIImage (@"Info/icon_phone.png");
-			var btnCall = UIButton.FromType (UIButtonType.Custom);
-			btnCall.Frame = new RectangleF (10, 380, 130, 60);
-			btnCall.SetBackgroundImage(imgCallButton, UIControlState.Normal);
-			btnCall.SetBackgroundImage(imgCallButton_pressed, UIControlState.Highlighted);
-			btnCall.SetImage (imgCallButtonIcon, UIControlState.Normal);
-			btnCall.ContentMode = UIViewContentMode.Center;
-			Add (btnCall);
+			_btnCall.Frame = new RectangleF (10, text.Frame.Bottom + 1, 130, 50);
+			_btnCall.SetBackgroundImage(imgButton, UIControlState.Normal);
+			_btnCall.SetBackgroundImage(imgButton_pressed, UIControlState.Highlighted);
+			_btnCall.SetImage (imgCallButtonIcon, UIControlState.Normal);
+			_btnCall.ContentMode = UIViewContentMode.Top;
+			_btnCall.TouchUpInside += (sender, e) => 
+			{
+				BeginInvokeOnMainThread (() => {new UIAlertView ("Phone", "7-777-2234455", null, "OK").Show ();}); 
+			};
+			Add (_btnCall);
 
-			var imgMailButton = new UIImage (@"Info/button.png").StretchableImage (11, 0);
-			var imgMailButton_pressed = new UIImage (@"Info/button_pressed.png").StretchableImage (11, 0);
 			var imgMailButtonIcon = new UIImage (@"Info/icon_mail.png");
-			var btnMail = UIButton.FromType (UIButtonType.Custom);
-			btnMail.Frame = new RectangleF (180, 380, 130, 60);
-			btnMail.SetBackgroundImage(imgMailButton, UIControlState.Normal);
-			btnMail.SetBackgroundImage(imgMailButton_pressed, UIControlState.Highlighted);
-			btnMail.SetImage (imgMailButtonIcon, UIControlState.Normal);
-			Add (btnMail);
-		}
-
-		public override void ViewWillAppear (bool animated) {
-			base.ViewWillAppear (animated);
-			this.NavigationController.SetNavigationBarHidden (true, animated);
-		}
-		public override void ViewWillDisappear (bool animated) {
-			base.ViewWillDisappear (animated);
-			this.NavigationController.SetNavigationBarHidden (false, animated);
+			_btnMail.Frame = new RectangleF (180, text.Frame.Bottom + 1, 130, 50);
+			_btnMail.SetBackgroundImage(imgButton, UIControlState.Normal);
+			_btnMail.SetBackgroundImage(imgButton_pressed, UIControlState.Highlighted);
+			_btnMail.SetImage (imgMailButtonIcon, UIControlState.Normal);
+			_btnMail.TouchUpInside += (sender, e) => 
+			{
+				BeginInvokeOnMainThread (() => {new UIAlertView ("Site", "www.Some.com", null, "OK").Show ();});
+			};
+			Add (_btnMail);
 		}
 	}
 }
